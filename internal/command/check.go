280ca09a469c7bddc0166fabf00d0766f2fe1e8a
Kensei Nakada <44139130+sanposhiho@users.noreply.github.com>
2021-03-25 03:43:20 +0900

    Revert "Feat: get source's file checksum and check if source is updated or not when mockgen/check (#44)" (#45)
    This reverts commit 3b40115e48e7eaf9d25537c3ea9787a749800223.

--- internal/command/check.go
+++ internal/command/check.go
@@ -41,14 +41,6 @@ func (r Runner) Check() {
 				runner = m.ReflectModeRunner
 			case model.SourceMode:
 				runner = m.SourceModeRunner
-				sourceChecksum, err := mockgen.SourceChecksum(runner)
-				if err != nil {
-					log.Fatalf("failed to calculate checksum of the source: %v", err)
-				}
-				if sourceChecksum == m.SourceChecksum {
-					// source file is not updated
-					return nil
-				}
 			default:
 				log.Printf("[WARN] unknown mock detected\nPlease reconfigure the mock. destination: %s", runner.GetDestination())
 				return nil
@@ -59,7 +51,7 @@ func (r Runner) Check() {
 				return fmt.Errorf("get checksum: %v", err)
 			}
 
-			if m.MockCheckSum != checksum {
+			if m.CheckSum != checksum {
 				// mock is not up to date
 				s := runner.GetSource()
 				d := runner.GetDestination()
@@ -75,7 +67,7 @@ func (r Runner) Check() {
 	}
 	err = g.Wait()
 	if err != nil {
-		log.Fatalf("failed to run: %v", err.Error())
+		log.Fatalf("failed to run mockgen: %v \nPlease run `%s` and check if mockgen works correctly with your options", err, r.MockgenRunner)
 	}
 	if isFail {
 		log.Fatal("mocks is not up-to-date")
--- internal/mockgen/runner.go
+++ internal/mockgen/runner.go
@@ -29,18 +29,9 @@ func Checksum(r Runner) ([16]byte, error) {
 		return [16]byte{}, fmt.Errorf("failed to run mockgen: %v \nPlease run `%s` and check if mockgen works correctly with your options", err, r)
 	}
 
-	checksum, err := util.CalculateCheckSum(tmpFile)
+	checksum, err := util.MockCheckSum(tmpFile)
 	if err != nil {
 		return [16]byte{}, fmt.Errorf("calculate checksum of the mock: %v", err)
-	}
-
-	return checksum, nil
-}
-
-func SourceChecksum(r Runner) ([16]byte, error) {
-	checksum, err := util.CalculateCheckSum(r.GetSource())
-	if err != nil {
-		return [16]byte{}, fmt.Errorf("calculate checksum of the mock source: %v", err)
 	}
 
 	return checksum, nil
